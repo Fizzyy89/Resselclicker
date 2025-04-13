a2547122de790a832e5e00958d1c6560c6254c8a
Fizzyy89 <jan.haeberle89@outlook.de>
2025-04-13 15:06:29 +0200

    new sprite

--- script.js
+++ script.js
@@ -1997,7 +1997,7 @@ function updateDatacenterLeaders() {
     leadersGrid.innerHTML = '';
     
     // Array mit verfügbaren Leiter-Sprites
-    const leaderSprites = ['images/rzleiter.png', 'images/rzleiter2.png', 'images/rzleiter3.png'];
+    const leaderSprites = ['images/rzleiter.png', 'images/rzleiter2.png', 'images/rzleiter3.png', 'images/rzleiter4.png'];
     
     // Berechne den Abstand zwischen Leitern basierend auf der Anzahl
     const containerWidth = leadersGrid.offsetWidth;
